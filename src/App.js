4ddef38a429d82898044aeda6efdc0d3365d4845
masatoomori <23582786+masatoomori@users.noreply.github.com>
2022-11-02 22:49:33 +0900

    2日目後半終了

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { List } from "./List"
 import { Form } from "./Form"
+import { LANGUAGES } from "./const/languages";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tab: 'list',
+      langs: LANGUAGES
     }
   }
+  addLang(lang) {
+    this.setState({
+      langs: [...this.state.langs, lang],
+      tab: 'list'
+    });
+  }
   render() {
-    const { tab } = this.state;
+    const { tab, langs } = this.state;
     return (
       <div>
         <header>
@@ -19,10 +27,11 @@ class App extends React.Component {
             <li onClick={() => this.setState({ tab: 'form' })}>フォーム</li>
           </ul>
         </header>
+        <hr />
         {
           tab === 'list' ?
-            <List />:
-            <Form />
+            <List langs={langs} />:
+            <Form onAddLang={(lang) => this.addLang(lang)} />
         }
       </div>
     )
